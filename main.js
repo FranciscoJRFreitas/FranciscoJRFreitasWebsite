a09e9f570a6d53f7b948a38b4e6490966d234731
Francisco Freitas <fj.freitas@campus.fct.unl.pt>
2025-02-16 14:49:20 +0000

    Song added

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ import "./style.css";
 import Experience from "./Experience/Experience.js";
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
+
+document.addEventListener("DOMContentLoaded", function () {
+    const audio = document.getElementById("background-audio");
+    const muteButton = document.getElementById("mute-button");
+    const muteIcon = muteButton.querySelector("i");
+
+    muteButton.addEventListener("click", function () {
+        if (audio.paused) {
+            audio.play();
+            muteIcon.classList.replace("fa-volume-mute", "fa-volume-up");
+        } else {
+            audio.pause();
+            muteIcon.classList.replace("fa-volume-up", "fa-volume-mute");
+        }
+    });
+});
